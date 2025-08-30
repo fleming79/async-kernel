0fdc661e0ae2791c7320e245b92d5614f2456c47
Alan Fleming <fleming79@users.noreply.github.com>
2025-08-30 14:35:10 +1000

    Enhancement -  Make kernel async enterable. (#77)
    * Make _start_in_context a private variable and make the kernel async enterable.
    
    * Remove unused setting  from CInt in test.
    
    * Revise docstring for Kernel.__aenter__.

--- tests/test_enter_kernel.py
+++ tests/test_enter_kernel.py
@@ -18,12 +18,12 @@ def anyio_backend(kernel_name: KernelName):
 
 
 async def test_start_kernel_in_context(anyio_backend, kernel_name):
-    async with Kernel().start_in_context() as kernel:
+    async with Kernel() as kernel:
         assert kernel.kernel_name == kernel_name
         connection_file = kernel.connection_file
         # Test prohibit nested async context.
         with pytest.raises(RuntimeError, match="Already started"):
-            async with kernel.start_in_context():
+            async with kernel:
                 pass
         # Test prevents binding socket more than once.
         with (
@@ -31,6 +31,6 @@ async def test_start_kernel_in_context(anyio_backend, kernel_name):
             kernel._bind_socket(SocketID.shell, None),  # pyright: ignore[reportArgumentType, reportPrivateUsage]
         ):
             pass
-    async with Kernel(connection_file=connection_file).start_in_context():
+    async with Kernel(connection_file=connection_file):
         # Test we can re-enter the kernel.
         pass
