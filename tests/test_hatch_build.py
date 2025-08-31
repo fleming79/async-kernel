e2c151e2503c894c6e4894fb9f7e5ee91fbabffd
Alan Fleming <>
2025-08-31 15:51:50 +1000

    Try alternate strategy for flaky test: test_hatch_buid_hook.

--- tests/test_hatch_build.py
+++ tests/test_hatch_build.py
@@ -2,15 +2,17 @@ import pathlib
 import shutil
 import subprocess
 
+import pytest
 
-async def test_hatch_buid_hook(anyio_backend, monkeypatch) -> None:
+
+@pytest.mark.flaky
+def test_hatch_buid_hook() -> None:
     base = pathlib.Path(__file__).parent.parent
-
     spec_folder = base.joinpath("data_kernelspec")
     shutil.rmtree(spec_folder, ignore_errors=True)
     folder = spec_folder.joinpath("async")
     assert not folder.exists()
     process = subprocess.Popen(["hatch", "build", "--hooks-only"])
-    assert process.wait(60) == 0
+    assert process.wait(timeout=60) == 0
     assert folder.exists()
     assert {f.name for f in folder.glob("*")} == {"logo-svg.svg", "kernel.json", "logo-32x32.png", "logo-64x64.png"}
