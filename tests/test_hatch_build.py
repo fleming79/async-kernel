e909cf733333b15a054613240013b19aca161c14
Alan Fleming <Alan.Fleming@utas.edu.au>
2025-08-31 16:38:24 +1000

    Further changes to test_hatch_buid_hook for windows compatibility.

--- tests/test_hatch_build.py
+++ tests/test_hatch_build.py
@@ -2,17 +2,24 @@ import pathlib
 import shutil
 import subprocess
 
+import anyio
 import pytest
 
 
 @pytest.mark.flaky
-def test_hatch_buid_hook() -> None:
+async def test_hatch_buid_hook(anyio_backend) -> None:
     base = pathlib.Path(__file__).parent.parent
     spec_folder = base.joinpath("data_kernelspec")
     shutil.rmtree(spec_folder, ignore_errors=True)
     folder = spec_folder.joinpath("async")
     assert not folder.exists()
-    process = subprocess.Popen(["hatch", "build", "--hooks-only"])
-    assert process.wait(timeout=60) == 0
-    assert folder.exists()
-    assert {f.name for f in folder.glob("*")} == {"logo-svg.svg", "kernel.json", "logo-32x32.png", "logo-64x64.png"}
+    subprocess.call(["hatch", "build", "--hooks-only"])
+    while not folder.exists():
+        await anyio.sleep(0.1)
+    await anyio.sleep(0.1)
+    assert {f.name for f in folder.glob(pattern="*")} == {
+        "logo-svg.svg",
+        "kernel.json",
+        "logo-32x32.png",
+        "logo-64x64.png",
+    }
