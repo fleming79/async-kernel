130ace39f86b5eb5365790d2c1afb596182f988c
Alan Fleming <>
2025-09-06 12:14:07 +1000

    Fix notes in Comm & CommManager docstrings.

--- src/async_kernel/comm.py
+++ src/async_kernel/comm.py
@@ -14,12 +14,13 @@ __all__ = ["Comm"]
 
 class Comm(BaseComm):
     """
-    `comm.BaseComms` with a [kernel][async_kernel.kernel.Kernel].
+    An implementation of `comm.BaseComms` for async-kernel.
 
-    Notes:
-    - `kernel` is added/removed by the CommManager.
-    - `kernel` is added to the CommManager by the kernel once the sockets have been opened.
-    - publish_msg is no-op when kernel is unset.
+    !!! note
+
+        - `kernel` is added/removed by the CommManager.
+        - `kernel` is added to the CommManager by the kernel once the sockets have been opened.
+        - publish_msg is no-op when kernel is unset.
     """
 
     __slots__ = [
@@ -68,12 +69,11 @@ class Comm(BaseComm):
 
 class CommManager(HasTraits, comm.base_comm.CommManager):  # pyright: ignore[reportUnsafeMultipleInheritance]
     """
-    A comm manager for Kernel (singleton).
+    The comm manager for all Comm instances.
 
-    When `kernel` is set the `kernel` on all existing `Comm` instances is also set.
+    !!! note
 
-    !!! note:
-
+        - When the trait `CommManager.kernel` is set the `Comm.kernel` trait is set on all [async_kernel.comm.Comm][] instances.
         - The `Comm` will only send messages when the kernel is set.
         - The `kernel` sets `CommManager.kernel` when its ready the iopub socket is open.
     """
@@ -116,8 +116,10 @@ def get_comm_manager():
 
 def set_comm():
     """
-    Set the [comm](https://pypi.org/project/comm/) module's `comm.create_comm` and `comm.get_com_manager` methods to
-    [async_kernel.comm.Comm][] and [async_kernel.comm.Comm][] respectively.
+    Monkey patch the [comm](https://pypi.org/project/comm/) module's functions to provide iopub comms.
+
+    1.  `comm.create_comm` to [async_kernel.comm.Comm][].
+    1. `comm.get_com_manager` to [async_kernel.comm.CommManager][].
     """
     comm.create_comm = Comm
     comm.get_comm_manager = get_comm_manager
