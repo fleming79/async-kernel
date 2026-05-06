ef1c7227e77184634e31b6e2b2d35b85f1a5fb5c
Alan Fleming <Alan.Fleming@utas.edu.au>
2026-05-06 15:29:51 +1000

    Include default argument in pack_json_bytes  and pack_json_str.

--- src/async_kernel/compat/json.py
+++ src/async_kernel/compat/json.py
@@ -9,8 +9,8 @@ __all__ = ["pack_json_bytes", "pack_json_str", "unpack_json"]
 if TYPE_CHECKING:
     from collections.abc import Callable
 
-    pack_json_bytes: Callable[[Any], bytes]
-    pack_json_str: Callable[[Any], str]
+    pack_json_bytes: Callable[[Any, Callable[[Any], Any]], bytes]
+    pack_json_str: Callable[[Any, Callable[[Any], Any]], str]
     unpack_json: Callable[[str | bytes], Any]
 
 
@@ -19,11 +19,11 @@ if importlib.util.find_spec("jupyter_client"):
 
     from jupyter_client.jsonutil import json_default
 
-    def _jc_pack_bytes(data: Any) -> bytes:
-        return json.dumps(data, default=json_default).encode()
+    def _jc_pack_bytes(data: Any, default=json_default) -> bytes:
+        return json.dumps(data, default=default).encode()
 
-    def _jc_pack_str(data: Any) -> str:
-        return json.dumps(data, default=json_default)
+    def _jc_pack_str(data: Any, default=json_default) -> str:
+        return json.dumps(data, allow_nan=False, default=default)
 
     pack_json_bytes, pack_json_str, unpack_json = _jc_pack_bytes, _jc_pack_str, json.loads
 
@@ -36,10 +36,10 @@ if importlib.util.find_spec("orjson"):
 
     ORJSON_OPTION = orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_NAIVE_UTC | orjson.OPT_UTC_Z
 
-    def _oj_pack_bytes(data) -> bytes:
-        return orjson.dumps(data, default=json_default, option=ORJSON_OPTION)
+    def _oj_pack_bytes(data, default=json_default) -> bytes:
+        return orjson.dumps(data, default=default, option=ORJSON_OPTION)
 
-    def _oj_pack_str(data) -> str:
-        return orjson.dumps(data, default=json_default, option=ORJSON_OPTION).decode()
+    def _oj_pack_str(data, default=json_default) -> str:
+        return orjson.dumps(data, default=default, option=ORJSON_OPTION).decode()
 
     pack_json_bytes, pack_json_str, unpack_json = _oj_pack_bytes, _oj_pack_str, orjson.loads
