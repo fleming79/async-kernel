d17b48b1ea5ba3ac492fa7a2d69b097728365154
Alan Fleming <>
2026-05-14 13:23:15 +1000

    Add tests for Kernel subclassing.

--- tests/test_kernel_subclass.py
+++ tests/test_kernel_subclass.py
@@ -2,11 +2,18 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
+
 from async_kernel.interface.zmq import ZMQKernelInterface
 from async_kernel.kernel import Kernel
 
 if TYPE_CHECKING:
     from async_kernel.typing import Backend
+
+
+def test_kernel_validators():
+    with pytest.raises(RuntimeError, match="An interface must be created prior to creating the kernel"):
+        Kernel()
 
 
 async def test_kernel_subclass(anyio_backend: Backend):
@@ -18,5 +25,28 @@ async def test_kernel_subclass(anyio_backend: Backend):
 
         async with ZMQKernelInterface() as kernel:
             assert isinstance(kernel, MyKernel)
+    finally:
+        Kernel._cls = None  # pyright: ignore[reportPrivateUsage]
+
+
+async def test_kernel_subclass_override(anyio_backend: Backend):
+    # Ensure the subclass correctly overrides the kernel.
+    try:
+
+        class MyKernelA(Kernel):
+            pass
+
+        class MyKernelB(Kernel):
+            pass
+
+        async with ZMQKernelInterface() as kernel:
+            assert isinstance(kernel, MyKernelB)
+            assert MyKernelB() is kernel
+            with pytest.raises(TypeError, match="A different kernel was loaded"):
+                MyKernelA()
+
+        async with ZMQKernelInterface(kernel_class=MyKernelA) as kernel:
+            assert isinstance(kernel, MyKernelA)
+
     finally:
         Kernel._cls = None  # pyright: ignore[reportPrivateUsage]
