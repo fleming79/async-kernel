fa993508df19c14e081a30066f33c1ae57187fe3
Alan Fleming <>
2026-08-16 20:45:10 +1000

    Make interface callers protected.

--- src/async_kernel/interface/base.py
+++ src/async_kernel/interface/base.py
@@ -390,7 +390,7 @@ class Interface(StartStopTask, Application, Generic[T_shell_co]):
         """The main task to run the kernel and open connections."""
         self.log.info("Starting kernel interface")
         self.backend = Backend(current_async_library())
-        async with self.kernel:
+        async with self.caller, self.callers[Channel.control], self.kernel:
             await self._pre_start()
             self.log.info("Interface started: %s", self.summary)
             started()
